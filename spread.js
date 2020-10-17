eade88a5b5888eaa6c29e112107effd15213c7d9
Nicholas Otieno <niklasoti@gmail.com>
2020-10-17 12:51:48 +0200

    spread operator fix

--- spread.js
+++ spread.js
@@ -1,6 +1,15 @@
 /*
- * Template literals
+ * Spread Operator
 */
+
+const arr1 = [4, 5, 6];
+const arr2 = [1, 2, 3, ...arr1];
+
+console.log(arr2);
+
+const arr2copy = [...arr2];
+
+console.log(arr2copy);
 
 
 
