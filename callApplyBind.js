aeff8d9ef808dd54f19f08340a69af481a2c923f
Nicholas Otieno <niklasoti@gmail.com>
2020-10-21 22:18:23 +0200

    Update

---
+++ callApplyBind.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/* CALL */
+// Functions are objects and have properties
+function foo(x,y) {
+  console.log(this)
+  x && y ? console.log(x + y) : null;
+}
+
+console.log(foo.name) // function name
+console.log(foo.length) // number of arguments
+
+// They have methods too (functions)
+console.log(foo.toString());
+
+foo.call(); // calls function like foo()
+
+foo.call({},4,2); // pass in what you wanr this to be folled by params
+
+/* APPLY */
+
+foo.apply(null,[1,2]); // simillar to call but we use array for params
+
+
+/* BIND */
+const a = function () {
+   console.log(this);
+}.bind({ name: 'nick'}) // only works on function expressions
+
+a();
+
+
+
+
+
+
+
+
+
+
+
