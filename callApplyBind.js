9631313ae4e52266170194225eee0722105a826c
Nicholas Otieno <niklasoti@gmail.com>
2020-10-23 07:42:59 +0200

    update

--- callApplyBind.js
+++ callApplyBind.js
@@ -2,9 +2,11 @@
 
 /* CALL */
 // Functions are objects and have properties
-function foo(x,y) {
+function foo(x, y) {
   console.log(this)
-  x && y ? console.log(x + y) : null;
+  if (x && y) {
+    console.log(x + y);
+  }
 }
 
 console.log(foo.name) // function name
@@ -15,17 +17,17 @@ console.log(foo.toString());
 
 foo.call(); // calls function like foo()
 
-foo.call({},4,2); // pass in what you wanr this to be folled by params
+foo.call({}, 4, 2); // pass in what you wanr this to be folled by params
 
 /* APPLY */
 
-foo.apply(null,[1,2]); // simillar to call but we use array for params
+foo.apply(null, [1, 2]); // simillar to call but we use array for params
 
 
 /* BIND */
 const a = function () {
-   console.log(this);
-}.bind({ name: 'nick'}) // only works on function expressions
+  console.log(this);
+}.bind({ name: 'nick' }) // only works on function expressions
 
 a();
 
